22dc327685ac2c414b40f0423ee4118d8c89f4ab
Marco Guido <marco@guidotorres.mx>
2023-10-23 15:59:47 -0400

    added 3d model rendering to hero canvas

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,6 @@
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
-import { twJoin } from "tailwind-merge";
 const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
